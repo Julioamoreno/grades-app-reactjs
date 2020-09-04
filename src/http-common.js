ac68b02f83b01a4fd19b429129997b890e3e22ba
Júlio Armando <julioarmando321@gmail.com>
2020-09-04 13:56:21 -0300

    Alterando variavel de ambiente para o padrao do react

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-	baseURL: process.env.BASE_URL_API,
+	baseURL: process.env.REACT_APP_BASE_URL_API,
 	headers: {
 		'Content-type': 'application/json',
 	},
